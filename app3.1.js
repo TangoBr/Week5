9704273dc481121f0b8aa713383d49a82db05818
TangoBr <thomasbridges01@gmail.com>
2021-02-20 21:49:47 -0500

    javaScript hard yo

---
+++ app3.1.js
@@ -0,0 +1,41 @@
+for (let counter = 0; counter <= 10; counter++) {
+    console.log(counter);
+}
+
+// while (true) {
+//     break;
+// }
+
+for (var bottlesOfBeer = 99; bottlesOfBeer > 0; bottlesOfBeer--) {
+    // break;
+    console.log(bottlesOfBeer + " bottles of beer on the wall. Take one down pass it around.");
+    // if (bottlesOfBeer = 1) {
+        // console.log(bottlesOfBeer + "bottle of beer left on the wall. Take it down pass it around")
+    }
+    // console.log(bottlesOfBeer);
+
+
+var bools = ["true", "true", "false", "true", "false", "true", "true", "true"];
+
+
+for (var i = 0; i < bools.length; i++){
+    // console.log(i) ;
+    if (bools[i] === "true") {
+        console.log(bools[i])
+    }
+    // console.log(bools[0])
+    // console.log(bools[1])
+    // console.log(bools[3])
+}
+
+var str = "apples"
+
+for (var i = 0; i < str.length; i++) {
+    if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u" ) {
+        console.log(str[i]);
+    }
+}
+
+// for (i = 0,; i < 3; )
+
+
